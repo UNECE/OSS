f86f734c7a652ede5de24ad4b1299a564c92a628
Andrew ("Andy") Tait <79048392+thoughthollow@users.noreply.github.com>
2025-02-10 09:55:33 +0100

    Pages updated with text corrections, index page icons' whitespace shrunk, and javascript fix implemented for index page contents alignment issue.

--- docs/assets/js/misc.js
+++ docs/assets/js/misc.js
@@ -1,4 +1,9 @@
-    $(document).ready(function ($) {
-        let openTab = window.location.hash;
-        $('.nav-link[href="' + openTab + '"]' ).trigger('click');
-    })
+const myTimeout = setTimeout(hideGlass, 1500);
+
+function hideGlass() {
+  var x = document.getElementById("quarto-sidebar-glass")
+  x.style.display = "none";
+/*  alert("I am an alert box!"); Test command*/
+}
+
+
