7b00d5a4c6f18a1277ba0e00bf10f02b908cb0c0
Andrew ("Andy") Tait <79048392+thoughthollow@users.noreply.github.com>
2025-02-10 10:00:18 +0100

    Javascript delay timer extended to 3 secs

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -1,8 +1,9 @@
-const myTimeout = setTimeout(hideGlass, 1500);
+const myTimeout = setTimeout(hideGlass, 3000);
 
 function hideGlass() {
   var x = document.getElementById("quarto-sidebar-glass")
   x.style.display = "none";
+   console.log("hideGlass script run (3 sec delay ver.)"); 
 /*  alert("I am an alert box!"); Test command*/
 }
 
