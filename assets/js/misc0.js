45086ce7e74ea4cfb03dea770b5273f321f39939
Andrew ("Andy") Tait <79048392+thoughthollow@users.noreply.github.com>
2025-09-26 17:45:36 +0200

    Added javascript to run command to remove a problematic div at a second earlier time to improve page loading.

---
+++ assets/js/misc0.js
@@ -0,0 +1,10 @@
+const myTimeout = setTimeout(hideGlass, 500);
+
+function hideGlass() {
+  var x = document.getElementById("quarto-sidebar-glass")
+  x.style.display = "none";
+   console.log("hideGlass script run (3 sec delay ver.)"); 
+/*  alert("I am an alert box!"); Test command*/
+}
+
+
